a5b05218e8b2229a03f343ac15b02f165cad5383
Pragalbha Patil <44872076+Pragalbha-Patil@users.noreply.github.com>
2020-06-19 22:14:03 +0530

    changed canvas size

--- snakegame/sketch.js
+++ snakegame/sketch.js
@@ -22,10 +22,10 @@ let w;
 let h;
 
 function setup() {
-  createCanvas(800, 600);
+  createCanvas(1024, 768);
   // Create the video
   video = createCapture(VIDEO);
-  video.size(640, 480);
+  video.size(320, 480);
   video.hide();
   // Mirro the video since we trained it that way!
   flipVideo = ml5.flipImage(video);
