8354829103c25b0c9c9e8eb15a7c73ab4ae49b33
Pragalbha <pragalbha77@gmail.com>
2024-07-01 22:46:20 +0530

    fixing scrolling up logic

--- one-million-checkboxes/script.js
+++ one-million-checkboxes/script.js
@@ -206,7 +206,7 @@ function loadMoreCheckboxes() {
 function isElementInViewport(el) {
     const rect = el.getBoundingClientRect();
     return (
-        rect.top >= 50 &&
+        rect.top >= 0 &&
         rect.left >= 0 &&
         rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
         rect.right <= (window.innerWidth || document.documentElement.clientWidth)
